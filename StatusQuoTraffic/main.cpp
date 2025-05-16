d2c53046ede56d5be859603a635046d7a4e70345
johqniel <daniel.nogues.daniel@gmail.com>
2025-05-16 11:22:56 +0200

    Initial Commit

---
+++ StatusQuoTraffic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StatusQuoTraffic
+//
+//  Created by nielo on 16.05.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
